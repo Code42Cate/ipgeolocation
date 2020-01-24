733c78403483b45aa46fa74b5e3ceba047dc12df
Jonas Scholz <jonas.scholz@bbscholz.de>
2020-01-24 22:50:13 +0100

    Added Options

--- Geolocation.go
+++ Geolocation.go
@@ -4,9 +4,93 @@ package ipgeolocation
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"net"
 	"net/http"
 	"net/url"
+	"reflect"
+	"strings"
 )
+
+// Options struct to build query in GetGeolocationWithOptions
+type Options struct {
+	IP       string
+	Language string
+	Exclude  []string
+	Include  []string
+}
+
+// Helper function to validate include/exclude fields
+func isDataField(field string) bool {
+	val := reflect.ValueOf(GeolocationData{})
+	for i := 0; i < val.Type().NumField(); i++ {
+		if val.Type().Field(i).Tag.Get("json") == field {
+			return true
+		}
+	}
+	return false
+}
+
+// SetIncludes calls SetInclude for each element. Returns non-nil error if one of the elements failed
+func (c *Options) SetIncludes(includes []string) error {
+	for _, include := range includes {
+		if err := c.SetInclude(include); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// SetInclude validates the field by checking if its a valid json field in the GeolocationData struct. Appends field to Options.Include if valid or returns non-nil error otherwise
+func (c *Options) SetInclude(include string) error {
+	if isDataField(include) {
+		c.Include = append(c.Include, include)
+		return nil
+	}
+	return errors.New("exclude is invalid")
+}
+
+// SetExcludes calls SetExclude for each element. Returns non-nil error if one of the elements failed
+func (c *Options) SetExcludes(excludes []string) error {
+	for _, exclude := range excludes {
+		if err := c.SetExclude(exclude); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// SetExclude validates the field by checking if its a valid json field in the GeolocationData struct. Appends field to Options.Exclude if valid or returns non-nil error otherwise
+func (c *Options) SetExclude(exclude string) error {
+	if isDataField(exclude) {
+		c.Exclude = append(c.Exclude, exclude)
+		return nil
+	}
+	return errors.New("exclude is invalid")
+}
+
+// SetLanguage validates the given language and sets it as Options attribute if valid. Returns non-nil error otherwise
+func (c *Options) SetLanguage(language string) error {
+	possibleLanguages := []string{"en", "de", "ru", "ja", "fr", "cn", "es", "cs", "it"}
+
+	for _, b := range possibleLanguages {
+		if b == language {
+			c.Language = language
+			return nil
+		}
+	}
+	return errors.New("language is not valid")
+}
+
+// Set the IP which you want to check. Validates the IP by trying to parse it with net.ParseIP
+func (c *Options) SetIP(IP string) error {
+	parsed := net.ParseIP(IP)
+	if parsed != nil {
+		c.IP = IP
+		return nil
+	}
+	return errors.New("ip got invalid format")
+}
 
 // GeolocationData represents the full response which we get from the API.
 // Full documentation: https://ipgeolocation.io/documentation/ip-geolocation-api.html
@@ -93,6 +177,59 @@ func (c *Client) GetGeolocation(ip string) (GeolocationData, error) {
 		return data, err
 	}
 	return data, nil
+}
+// GetGeolocationWithOptions, GetGeolocation but with more options!:D
+func (c *Client) GetGeolocationWithOptions(options Options) (GeolocationData, error) {
+	var data GeolocationData
+
+	u, _ := url.Parse(baseURL)
+
+	addQuery(u, "apiKey", c.ClientToken)
+
+	// IP
+	if options.IP != "" {
+		addQuery(u, "ip", options.IP)
+	}
+
+	includes := strings.Join(options.Include, ",")
+	if includes != "" {
+		addQuery(u, "fields", includes)
+	}
+	fmt.Println(includes)
+
+	excludes := strings.Join(options.Exclude, ",")
+	if excludes != "" {
+		addQuery(u, "excludes", excludes)
+	}
+
+	if options.Language != "" {
+		addQuery(u, "lang", options.Language)
+	}
+
+	res, err := http.Get(u.String())
+	if err != nil {
+		return data, err
+	}
+
+	switch res.StatusCode {
+	case 400:
+		return data, errors.New("your subscription is paused from use")
+	case 401:
+		return data, errors.New("401 error code, permission denied")
+	case 403:
+		return data, errors.New("ip address or domain is not valid")
+	case 404:
+		return data, errors.New("ip not found in database")
+	case 423:
+		return data, errors.New("private ip address")
+	}
+
+	err = json.NewDecoder(res.Body).Decode(&data)
+	if err != nil {
+		return data, err
+	}
+	return data, nil
+
 }
 
 func addQuery(url *url.URL, key string, value string) {
--- README.md
+++ README.md
@@ -24,19 +24,38 @@ func main() {
 	c := ipgeolocation.Client{}
 	c.SetToken("your token")
 
-	// Leave the IP empty if you want to check your host IP
-	ip := ""
-	data, err := c.GetGeolocation(ip)
+	// Get all the data for your IP
+	d, err := c.GetGeolocation("") // Or change it to any other IP
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(data)
+	// You can also specify more options:
+	options := Options{
+		IP:       "", // No IP <=> your IP
+		Language: "de",
+		Exclude:  []string{},       // You can set excludes here, or with the setter functions
+		Include:  []string{"city"}, // Same as for excludes
+	}
+
+	options.SetInclude("ISP")
+	// Or you could also just do:
+	// options.SetIncludes("ISP, "city")
+	options.SetLanguage("en") // Will overwrite "de"
+
+	d, err = c.GetGeolocationWithOptions(options)
+	fmt.Println(d) // You will still get the complete struct, just with default values in the fields you did not include.
+
+	options = Options{}
+	options.SetExcludes([]string{"currency", "time_zone", "country_flag", "geoname_id"})
+
+	d, err = c.GetGeolocationWithOptions(options)
+	fmt.Println(d) // You will still get the complete struct, just with default values in the fields you excluded.
 }
 ```
 
 #### Planned Additions
 
-- Adding all the other features which ipgeolocation.io provides
-- Unit tests
+- Adding premium features which ipgeolocation.io provides
+- Unit tests?
